4620f2958873ef5afca8a1de5eef068ec958848e
Anthony Onwuli <30937913+AntObi@users.noreply.github.com>
2024-02-25 22:22:33 +0000

    Fix `test_doper.py`

--- smact/tests/test_doper.py
+++ smact/tests/test_doper.py
@@ -2,23 +2,14 @@ import unittest
 
 import smact
 from smact.dopant_prediction import doper
-from smact.structure_prediction import mutation, utilities
+from smact.structure_prediction import utilities
 
 
 class dopant_prediction_test(unittest.TestCase):
     def test_dopant_prediction(self):
         num_dopants = 10
-        test_specie = ("Cu+", "Ga3+", "S2-")
+        test_specie = ("Cu1+", "Ga3+", "S2-")
         test = doper.Doper(test_specie)
-
-        cation_max_charge = max(
-            test_specie, key=lambda x: utilities.parse_spec(x)[1]
-        )
-        anion_min_charge = min(
-            test_specie, key=lambda x: utilities.parse_spec(x)[1]
-        )
-        _, cat_charge = utilities.parse_spec(cation_max_charge)
-        _, an_charge = utilities.parse_spec(anion_min_charge)
 
         # Assert: Length of the list and return type (dictionary: list)
         self.assertIs(type(test.get_dopants()), dict)
@@ -31,12 +22,24 @@ class dopant_prediction_test(unittest.TestCase):
         n_sub_list_an = test.get_dopants().get("n-type anion substitutions")
         p_sub_list_an = test.get_dopants().get("p-type anion substitutions")
         for n_atom, p_atom in zip(n_sub_list_cat, p_sub_list_cat):
-            self.assertGreater(utilities.parse_spec(n_atom[0])[1], cat_charge)
-            self.assertLess(utilities.parse_spec(p_atom[0])[1], cat_charge)
+            self.assertGreater(
+                utilities.parse_spec(n_atom[0])[1],
+                utilities.parse_spec(n_atom[1])[1],
+            )
+            self.assertLess(
+                utilities.parse_spec(p_atom[0])[1],
+                utilities.parse_spec(p_atom[1])[1],
+            )
 
         for n_atom, p_atom in zip(n_sub_list_an, p_sub_list_an):
-            self.assertGreater(utilities.parse_spec(n_atom[0])[1], an_charge)
-            self.assertLess(utilities.parse_spec(p_atom[0])[1], an_charge)
+            self.assertGreater(
+                utilities.parse_spec(n_atom[0])[1],
+                utilities.parse_spec(n_atom[1])[1],
+            )
+            self.assertLess(
+                utilities.parse_spec(p_atom[0])[1],
+                utilities.parse_spec(p_atom[1])[1],
+            )
 
 
 if __name__ == "__main__":
